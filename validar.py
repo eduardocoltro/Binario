52a27f32912eeb9dcc08ab4a32ed0a1b2725586c
Eduardo Augusto Coltro <94021551+eduardocoltro@users.noreply.github.com>
2022-03-24 13:55:19 -0300

    Update validar.py

--- validar.py
+++ validar.py
@@ -1,29 +1,30 @@
 class Pilha:
     def __init__(self):
-        self._vet = [] # lista interna
+        self._vet = []
 
-    def peek(self): # retorna qual item esta no topo
-        return self._vet[-1]
+    def peek(self):
+        if len(self._vet) > 0:
+            return self._vet[-1]
        
-    def push(self, item): # metodo de inserir item
+    def push(self, item):
         self._vet.append(item)
         
-    def pop(self): # remover o topo e retornar item para usuario
+    def pop(self):
         # tratar caso de underflow
         if self.is_empty():
             print("Lista Vazia!")
             return None
         return self._vet.pop()
                 
-    def is_empty(self): # teste se é vazia
+    def is_empty(self):
         if len(self._vet) > 0:
             return False
         return True
         
-    def __len__(self): # retorna o total de itens
+    def __len__(self):
         return len(self._vet)
 
-    def __str__(self): # representacao da pilha como string
+    def __str__(self):
         return str(self._vet)
 
 __name__ == "__main__"
@@ -33,27 +34,22 @@ sI = ["(", "{", "["]
 sF = [")", "}", "]"]
 
 for i in range(len(expressao)):
-        if expressao[i] in sI:
-            p.push(expressao[i])
-        elif expressao[i] in sF:
-            if p.is_empty():
-             print("sua expressão está inválida")
-            break
-        else:
-            if expressao[i] == ")" and p.peek() == "(":
-                p.pop()
-            elif expressao[i] == "[" and p.peek() == "]":
-                p.pop()
-            elif expressao[i] == "{" and p.peek() == "}":
-                p.pop()
-            else:
-                print("expressao invalida!")
-                break
-            if len(p) == 0:
-                print("expressao valida!")
-                break
+    if expressao[i] in sI:
+        p.push(expressao[i])
+    if  expressao[i] in sF:
+        if expressao[i] == ")" and p.peek() == "(":
+            p.pop()
+        elif expressao[i] == "]" and p.peek() == "[":
+            p.pop()
+        elif expressao[i] == "}" and p.peek() == "{":
+            p.pop()    
+
+print(p)
+
 if p.is_empty():
-    print("expressao valida!")
+    print("expressao válida!")
+else:
+    print("expressao inválida!")
 
 
 
